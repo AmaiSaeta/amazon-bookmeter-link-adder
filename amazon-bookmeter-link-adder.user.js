57a4a8d31b1c6c525d856f760c88a2b132d159f0
AmaiSaeta <amaisaeta@gmail.com>
2013-04-25 13:51:52 +0900

    Changed version number.

--- amazon-bookmeter-link-adder.user.js
+++ amazon-bookmeter-link-adder.user.js
@@ -2,7 +2,7 @@
 // @name		Amazon.co.jp - Bookmeter Link Adder
 // @namespace		http://amaisaeta.seesaa.net/
 // @author		AmaiSaeta (original script author: Jankokutou)
-// @version		1.00.20121204
+// @version		1.01.20130425
 // @description		Amazon.co.jpの商品ページに読書メーターへのリンクを追加する。
 // @include		http://www.amazon.co.jp/exec/obidos/ASIN/*
 // @include		http://www.amazon.co.jp/*/dp/*
